0b40fd4213b872e24820973d9c8d0f29a1594384
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-03-23 16:29:16 -0230

    Add last-command information to fish_right_prompt.
    Output the exit status and runtime of the last-executed command in the
    right-side prompt.

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,19 @@
 # Defined in /tmp/fish.2RFuzK/fish_right_prompt.fish @ line 2
 function fish_right_prompt
-    set_color $fish_color_end
-    date +%H:%M:%S
+    set -l last_status $status
+    if test $last_status = 0
+        set_color $fish_color_end
+    else
+        set_color $fish_color_error
+    end
+    echo -n $last_status
+
+    if test $CMD_DURATION
+        set dur (echo "$CMD_DURATION 1000" | awk '{printf "%.2fs", $1 / $2}')
+
+        set_color $fish_color_operator
+        echo -n ':'(set_color $fish_color_end)$dur
+    end
+
     echo -n ' '
 end
