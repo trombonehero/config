b87a2c91ef8d55b025c2e8cbbaaa8d0037a11079
Jonathan Anderson <jonathan.anderson@mun.ca>
2024-02-06 12:29:05 -0330

    Redirect stderr from which too.

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -2,7 +2,7 @@ function ls --wraps=exa --description 'List contents of directory'
   isatty stdout
   and set -a opt -F
 
-  if which exa > /dev/null
+  if which exa > /dev/null 2> /dev/null
     exa --git $argv
   else
     /bin/ls $argv
