a2718f1fca9dcfb58ce6daaf195830f8983fc1d1
Jonathan Anderson <jonathan.anderson@mun.ca>
2025-05-14 12:20:58 -0230

    Use gruvbox theme for rvc, too.

--- .config/fish/functions/rvc.fish
+++ .config/fish/functions/rvc.fish
@@ -1,3 +1,4 @@
 function rvc
-	clang --target=riscv32 -march=rv32i -S $argv -o - | pygmentize -l asm
+	clang --target=riscv32 -march=rv32i -S $argv -o - \
+		| pygmentize -O style=gruvbox-dark -l asm
 end
